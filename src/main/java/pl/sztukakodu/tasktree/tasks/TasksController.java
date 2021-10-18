07cbb103b4d5dcf1c89577ce08ef6c58f734caee
Leszek Rossa <leszek.rossa@gmail.com>
2021-10-19 01:35:48 +0200

    Module 3 - lessons 3-4
    - added abillity to auto live reload on build  project by adding Spring Developers Tools
    - added "/test" endpoint - for check if live reload is working
    - updated main endpoint "/" to "/api/tasks" + added methods handling HTTP PUT and HTTP DELETE
    - test requests HTTP - updated endpoint path + added test requests for each HTTP method
    
    (cherry picked from commit 15b1927b7691658f5a74241ad4d868cb7f1a565b)

--- src/main/java/pl/sztukakodu/tasktree/tasks/TasksController.java
+++ src/main/java/pl/sztukakodu/tasktree/tasks/TasksController.java
@@ -1,13 +1,14 @@
 package pl.sztukakodu.tasktree.tasks;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
-@RequestMapping(path = "/")
+@RequestMapping(path = "/api/tasks")
 @Slf4j
 public class TasksController {
 
@@ -30,9 +31,24 @@ public class TasksController {
         return tasksRepository.fetchAll();
     }
 
+    @GetMapping(path = "/hotReload")
+    public String test() {
+        return "live reload on build project WORKS!";
+    }
+
     @PostMapping
     public void addTask(@RequestBody Task task) {
         log.info(config.getEndpointPostLogMessage(), task);
         tasksRepository.addTask(task);
+    }
+
+    @PutMapping
+    public void updateTask() {
+        log.info("task updated");
+    }
+
+    @DeleteMapping
+    public void deleteTask() {
+        log.info("task deleted");
     }
 }
